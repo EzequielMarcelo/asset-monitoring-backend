5281255295d105be7cda36ec977ef7abe66ef3e7
Marcelo Ezequiel <marcelogustavo05@hotmail.com>
2026-05-31 20:50:01 -0300

    fix(rule): estava faltando o campo de id da variável nos schemas.

--- app/api/schemas/rule.py
+++ app/api/schemas/rule.py
@@ -3,8 +3,10 @@ from pydantic import BaseModel
 class RuleCreate(BaseModel):
     operator: str
     value: float
+    variable_id: int
 
 class RuleResponse(BaseModel):
     id: int
     operator: str
     value: float
+    variable_id: int
